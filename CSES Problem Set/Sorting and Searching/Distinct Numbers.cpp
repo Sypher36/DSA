142043c5f3e09e6b0407c5231e1eed6a1efa70fa
Suyog Anil Kasture <kasturesuyog@gmail.com>
2025-04-14 11:25:42 +0530

    Added searching and sorting algorithm solutions

---
+++ CSES Problem Set/Sorting and Searching/Distinct Numbers.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    set<int> s;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    cout << s.size() << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        solve();
+    }
+    return 0;
+}
